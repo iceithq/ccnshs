a50139e240bff7d6436b47cfe70344521853c4b3
Ian <ian.escarro@gmail.com>
2024-08-09 06:32:06 +0800

    update  font

--- application/views/admin/home/register.php
+++ application/views/admin/home/register.php
@@ -2,22 +2,22 @@
 
 <div class="register">
   <img src="public/themes/default/img/logo.png">
-    <h3>Register</h3>
+  <h3>Register</h3>
   <?php echo form_open('register'); ?>
   <p>Email<br>
-    <?php echo form_input('email', $this->input->post('email')); ?>
+    <?php echo form_input('email', $this->input->post('email'), 'class="form-control"'); ?>
     <?php echo form_error('email'); ?>
   </p>
   <p>Password<br>
-    <?php echo form_password('password'); ?>
+    <?php echo form_password('password', '', 'class="form-control"'); ?>
     <?php echo form_error('password'); ?>
   </p>
   <p>Confirm password<br>
-    <?php echo form_password('confirm_password'); ?>
+    <?php echo form_password('confirm_password', '', 'class="form-control"'); ?>
     <?php echo form_error('confirm_password'); ?>
   </p>
   <p>
-    <?php echo form_submit('submit', 'Save changes'); ?>
+    <?php echo form_submit('submit', 'Register', 'class="btn btn-success"'); ?>
   </p>
   <p>
     Already have an account? <?php echo anchor('login', 'Log in'); ?>
@@ -30,6 +30,7 @@
     width: 300px;
     margin: auto;
   }
+
   .register img {
     width: 128px;
   }
