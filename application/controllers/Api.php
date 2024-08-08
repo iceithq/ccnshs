471e15f3ad4f2d56c85fb272da88bacc8c0bb27b
Ian <ian.escarro@gmail.com>
2024-08-08 08:35:23 +0800

    fix hello api

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,13 +1,14 @@
 <?php
 
 class Api extends CI_Controller {
-  
+
   function __construct() {
     parent::__construct();
     header('Content-Type: application/json');
   }
 
   function hello() {
-    echo json_encode('hello world');
+    echo json_encode(array('message' => 'hello world'));
   }
+
 }
