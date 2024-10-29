669a4f74fdcb319c795839dab04edeaf39b8e8a6
Ian <ian.escarro@gmail.com>
2024-10-29 09:18:15 +0800

    Update hero

--- application/views/ccnshs/home.php
+++ application/views/ccnshs/home.php
@@ -24,16 +24,20 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="public/themes/ccnshs/img/hero/01.png" alt="Second slide">
+      <img class="d-block w-100" src="public/themes/ccnshs/img/hero/01.png?v1">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="public/themes/ccnshs/img/hero/02.png" alt="First slide">
+      <img class="d-block w-100" src="public/themes/ccnshs/img/hero/02.png?v1">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="public/themes/ccnshs/img/hero/03.png">
+      <img class="d-block w-100" src="public/themes/ccnshs/img/hero/03.png?v1">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="public/themes/ccnshs/img/hero/04.png?v1">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
